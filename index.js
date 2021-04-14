b465e30981b6106e17c877521f2f95553c455b9a
bouba93 <boubaker.kalfat@gmail.com>
2021-04-14 01:15:20 +0000

    Added default export

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import ImageWithPlaceholder from './src/ImageWithPlaceholder/ImageWithPlaceholder';
-export { ImageWithPlaceholder };
+import ImageWithPlaceholder from "./src/ImageWithPlaceholder/ImageWithPlaceholder";
+export default ImageWithPlaceholder;
